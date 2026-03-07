d0c544e156a23c4eeb30bd00d2ef89d0163464f4
VincentRasskazov <VincentRasskazov@users.noreply.github.com>
2026-03-07 23:50:52 +0000

    deploy: 6747fdf237b385ec851ccc565cd08288fd16871b

--- precache-manifest.1d743cf8e1a0c570dd0d01551caffe22.js
+++ precache-manifest.1d743cf8e1a0c570dd0d01551caffe22.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f292e57f2adb42318ffd9827a47b17e9",
+    "revision": "0b33236a56cf2b00f13cfdefb5a7cc92",
     "url": "/index.html"
   },
   {
-    "revision": "4c8356bf697b0ef86d49",
+    "revision": "effb160be9763c7d04e5",
     "url": "/static/css/main.5861a32c.chunk.css"
   },
   {
-    "revision": "694f1f1da0810a40587f",
-    "url": "/static/js/2.7ddc0f7e.chunk.js"
+    "revision": "de2df6b6ed8714e35059",
+    "url": "/static/js/2.198ac846.chunk.js"
   },
   {
     "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
-    "url": "/static/js/2.7ddc0f7e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.198ac846.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4c8356bf697b0ef86d49",
-    "url": "/static/js/main.62beb6d8.chunk.js"
+    "revision": "effb160be9763c7d04e5",
+    "url": "/static/js/main.5d29f405.chunk.js"
   },
   {
     "revision": "9bc2349bfbe76e87395f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6492b3c781a686662682ecedc27cec08.js"
+  "/precache-manifest.1d743cf8e1a0c570dd0d01551caffe22.js"
 );
 
 self.addEventListener('message', (event) => {
