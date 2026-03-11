aa5962e138ceea1d99a5202c4607d4bcc0edf3b2
VincentRasskazov <VincentRasskazov@users.noreply.github.com>
2026-03-11 04:55:02 +0000

    deploy: 04882e264a4cea3f21bcb78070e810cee8ea5a7a

--- precache-manifest.632e9358aa37b962d5d1cdd582e826b8.js
+++ precache-manifest.632e9358aa37b962d5d1cdd582e826b8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "44801e6a78a02053e1465c856731252f",
+    "revision": "9b094ffbf36203a20671c17fa1c4acf4",
     "url": "/talk/index.html"
   },
   {
-    "revision": "c2ee26eafec1c2f11dfc",
+    "revision": "d105005006ffc7a46d25",
     "url": "/talk/static/css/main.b4d34d15.chunk.css"
   },
   {
-    "revision": "b2988ef1e1b80a0bbfcf",
-    "url": "/talk/static/js/2.fe46b544.chunk.js"
+    "revision": "b88983e720aa1c918f21",
+    "url": "/talk/static/js/2.5aead6cb.chunk.js"
   },
   {
     "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
-    "url": "/talk/static/js/2.fe46b544.chunk.js.LICENSE.txt"
+    "url": "/talk/static/js/2.5aead6cb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c2ee26eafec1c2f11dfc",
-    "url": "/talk/static/js/main.53dde240.chunk.js"
+    "revision": "d105005006ffc7a46d25",
+    "url": "/talk/static/js/main.0b61e003.chunk.js"
   },
   {
     "revision": "d3b373b65ff0b3c2696e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/talk/precache-manifest.e71a319987798a76f20ba85ed1ed51a6.js"
+  "/talk/precache-manifest.632e9358aa37b962d5d1cdd582e826b8.js"
 );
 
 self.addEventListener('message', (event) => {
